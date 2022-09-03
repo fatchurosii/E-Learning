d8355ca89f3c73b006afdc75148c0037d27ffacc
fatchurrosii <watashifrdesu@yahoo.com>
2022-09-03 10:04:11 +0700

    add section mentor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,26 +62,26 @@
 
 <body class="antialiased ">
 
-    <section id="#home" class="lg:pt-28 pt-24 md:pt-20  max-w-full lg:pb-36 pb-20">
+    <section id="#home" class="max-w-full pt-24 pb-20 lg:pt-28 md:pt-20 lg:pb-36">
         <div class="container">
             <div class="flex flex-wrap">
                 <div class="self-center w-full px-4 lg:w-1/2">
                     <h1 class="text-3xl font-semibold text-primary md:text-4xl lg:text-4xl">
                         Bimbel Bahasa Inggris untuk anak - anak dengan Mentor yang berpengalaman
                     </h1>
-                    <h2 class="py-5 mb-5 font-medium text-gray-400 lg:text-lg text-sm">
+                    <h2 class="py-5 mb-5 text-sm font-medium text-gray-400 lg:text-lg">
                         Dengan bergabung dengan Naladhipa Courses anak anda akan merasakan pengalaman yang seru saat
                         belajar bahasa inggris
                     </h2>
                     <div class="items-center justify-between">
-                        <ul class="flex flex-row lg:md:space-x-10 space-x-5 py-10">
+                        <ul class="flex flex-row py-10 space-x-5 lg:md:space-x-10">
                             <li>
                                 <a href="#"
-                                    class="px-10 py-4 font-semibold text-xl text-white rounded-xl bg-primary">
+                                    class="px-10 py-4 text-xl font-semibold text-white rounded-xl bg-primary">
                                     Yuk Gabung
                                 </a>
                             </li>
-                            <li class="text-dark text-lg font-semibold -mt-3">
+                            <li class="-mt-3 text-lg font-semibold text-dark">
                                 <span class="px-3">10+</span>
                                 <br>
                                 <span>Siswa</span>
@@ -90,23 +90,69 @@
                     </div>
                 </div>
                 <div class="self-end w-full px-4 lg:w-1/2 lg:py-0 ">
-                    <img src="{{ asset('/assets/img/Hero.png') }}" class="lg:block hidden" alt="">
+                    <img src="{{ asset('/assets/img/Hero.png') }}" class="hidden lg:block" alt="">
                 </div>
             </div>
         </div>
     </section>
 
-    <section id="#feature" class="bg-secondary lg:max-w-full py-10 ">
+    <section id="#feature" name="feature" class="py-5 lg:py-10 lg:pb-20 bg-secondary lg:max-w-full">
         <div class="container">
-            <div class="text-center lg:px-72 lg:py-20 py-8 lg:mb-5 mb-10">
-                <h1 class="text-2xl lg:text-4xl font-semibold text-primary"> Naladhipa Courses merupakan tempat
+            <div class="py-8 mb-10 text-center lg:px-72 lg:py-20 lg:mb-5">
+                <h1 class="text-2xl font-semibold lg:text-4xl text-primary"> Naladhipa Courses merupakan tempat
                     terbaik
                     untuk bimbel
                     Bahasa Inggris </h1>
             </div>
-            <div class="lg:flex flex-wrap gap-y-10 lg:px-10 px-3">
-                <div class="lg:w-4/12 md:w-6/12">
-                    <div class="p-6 max-w-sm bg-primary rounded-xl shadow-lg border border-gray-200">
+            <div class="flex-wrap px-3 py-5 lg:py-20 lg:flex gap-y-10 lg:px-10">
+                <div class="pb-5 lg:w-4/12 md:w-6/12">
+                    <div class="max-w-sm p-6 border border-gray-200 shadow-lg bg-primary rounded-xl">
+                        <div class="items-center justify-between">
+                            <ul class="flex flex-row space-x-5">
+                                <li>
+                                    <img src="{{ asset('/assets/img/Time-Circle.svg') }}" class="w-16 h-16"
+                                        alt="time">
+                                </li>
+                                <li class="mt-5">
+                                    <a href="#">
+                                        <h5
+                                            class="mb-2 text-2xl font-semibold tracking-tight text-white dark:text-white">
+                                            Terjangkau</h5>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <p class="pt-5 mb-3 text-lg font-normal text-white dark:text-gray-400">Dengan harga mulai 100
+                            ribu perbulan anak anda sudah bisa mengikuti bimbel disini.</p>
+
+                    </div>
+
+                </div>
+                <div class="pb-5 lg:w-4/12 md:w-6/12">
+                    <div class="max-w-sm p-6 border border-gray-200 shadow-lg bg-primary rounded-xl">
+                        <div class="items-center justify-between">
+                            <ul class="flex flex-row space-x-5">
+                                <li>
+                                    <img src="{{ asset('/assets/img/Time-Circle.svg') }}" class="w-16 h-16"
+                                        alt="time">
+                                </li>
+                                <li class="mt-5">
+                                    <a href="#">
+                                        <h5
+                                            class="mb-2 text-2xl font-semibold tracking-tight text-white dark:text-white">
+                                            Flexible</h5>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <p class="pt-5 mb-3 text-lg font-normal text-white dark:text-gray-400">Pembelajaran dengan waktu
+                            yang flexible, orang tua dapat memilih jadwal bimbel</p>
+
+                    </div>
+
+                </div>
+                <div class="pb-5 lg:w-4/12 md:w-6/12">
+                    <div class="max-w-sm p-6 border border-gray-200 shadow-lg bg-primary rounded-xl">
                         <div class="items-center justify-between">
                             <ul class="flex flex-row space-x-5">
                                 <li>
@@ -122,55 +168,7 @@
                                 </li>
                             </ul>
                         </div>
-                        <p class="mb-3 text-lg font-normal text-white dark:text-gray-400 pt-5">Dengan menggunakan
-                            metode Fun
-                            Learning, siswa dapat lebih cepat belajar</p>
-
-                    </div>
-
-                </div>
-                <div class="lg:w-4/12 md:w-6/12">
-                    <div class="p-6 max-w-sm bg-primary rounded-xl shadow-lg border border-gray-200">
-                        <div class="items-center justify-between">
-                            <ul class="flex flex-row space-x-5">
-                                <li>
-                                    <img src="{{ asset('/assets/img/Time-Circle.svg') }}" class="w-16 h-16"
-                                        alt="time">
-                                </li>
-                                <li class="mt-5">
-                                    <a href="#">
-                                        <h5
-                                            class="mb-2 text-2xl font-semibold tracking-tight text-white dark:text-white">
-                                            Fun Learning</h5>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <p class="mb-3 text-lg font-normal text-white dark:text-gray-400 pt-5">Dengan menggunakan
-                            metode Fun
-                            Learning, siswa dapat lebih cepat belajar</p>
-
-                    </div>
-
-                </div>
-                <div class="lg:w-4/12 md:w-6/12">
-                    <div class="p-6 max-w-sm bg-primary rounded-xl shadow-lg border border-gray-200">
-                        <div class="items-center justify-between">
-                            <ul class="flex flex-row space-x-5">
-                                <li>
-                                    <img src="{{ asset('/assets/img/Time-Circle.svg') }}" class="w-16 h-16"
-                                        alt="time">
-                                </li>
-                                <li class="mt-5">
-                                    <a href="#">
-                                        <h5
-                                            class="mb-2 text-2xl font-semibold tracking-tight text-white dark:text-white">
-                                            Fun Learning</h5>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                        <p class="mb-3 text-lg font-normal text-white dark:text-gray-400 pt-5">Dengan menggunakan
+                        <p class="pt-5 mb-3 text-lg font-normal text-white dark:text-gray-400">Dengan menggunakan
                             metode Fun
                             Learning, siswa dapat lebih cepat belajar</p>
 
@@ -178,6 +176,33 @@
 
                 </div>
 
+            </div>
+        </div>
+    </section>
+
+    <section id="#about" name="about" class="py-10 bg-white lg:py-20 ">
+        <div class="container">
+            <div class="px-4 text-left">
+                <h1 class="pb-2 text-xl font-semibold lg:text-3xl text-primary">
+                    Kenalan yuk sama Mentor di
+                </h1>
+                <h1 class="text-3xl font-semibold lg:text-4xl text-primary">
+                    Naladhipa Course
+                </h1>
+            </div>
+            <div class="flex-wrap px-3 py-5 lg:px-4 lg:py-10 lg:flex gap-y-10 ">
+                <div class="items-center self-center pb-10 lg:w-6/12">
+                    <img src="{{ asset('/assets/img/Mentor.png') }}" class="max-w-xs lg:max-w-lg "alt="">
+                </div>
+                <div class="items-center self-center lg:w-6/12">
+                    <h1 class="py-2 text-4xl font-semibold">Mentor Albert</h1>
+                    <h1 class=" text-dark lg:text-2xl">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting, remaining essentially unchanged
+                    </h1>
+                </div>
             </div>
         </div>
     </section>
