22a2d934821ed3af663e7f1b9ab1a466f11ec56d
fatchurrosii <watashifrdesu@yahoo.com>
2022-09-12 13:43:15 +0700

    add side navbar blade

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html class="scroll-smooth">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+</head>
+
+<body class="bg-gray-50">
+    <div class="relative flex min-h-screen">
+        <aside class="w-64 p-6 bg-white shadow-md shadow-slate-200">
+            <h1
+                class="w-full px-4 py-4 mb-5 text-base font-medium text-center bg-white border-b border-gray-100 cursor-pointer text-primary shadow-gray-50">
+                Naladhipa Courses</h1>
+            <nav class="flex flex-col px-4 pb-4 my-4 ">
+                <div class="border-b border-gray-100">
+                    <a href=""
+                        class="mb-4  py-2.5 px-4 hover:bg-primary hover:text-white rounded-md cursor-pointer flex items-center gap-x-3"><svg
+                            class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                            <path
+                                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z">
+                            </path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222">
+                            </path>
+                        </svg><span class="text-lg">Siswa</span></a>
+                    <a href=""
+                        class="mb-4  py-2.5 px-4  hover:bg-primary hover:text-white rounded-md cursor-pointer flex items-center gap-x-3"><svg
+                            class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
+                            </path>
+                        </svg><span class="text-lg">Mentor</span></a>
+                    <a href=""
+                        class="mb-4   py-2.5 px-4  hover:bg-primary hover:text-white rounded-md cursor-pointer  flex items-center gap-x-3"><svg
+                            class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253">
+                            </path>
+                        </svg><span class="text-lg">Kelas</span></a>
+                    <a href=""
+                        class="mb-4 text-base  py-2.5 px-4  hover:bg-primary hover:text-white rounded-md cursor-pointer flex items-center gap-x-3"><svg
+                            class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z">
+                            </path>
+                        </svg> <span class="text-lg "> Rapor</span></a>
+                </div>
+
+                <div class="my-4">
+                    <a href=""
+                        class="mb-4 my-4 text-base  py-2.5 px-4  hover:bg-primary hover:text-white rounded-md cursor-pointer flex items-center gap-x-3"><svg
+                            class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1">
+                            </path>
+                        </svg> <span class="text-lg ">Logout</span></a>
+                </div>
+            </nav>
+        </aside>
+        <div class="w-full px-4">
+            <div class="min-h-screen bg-white">
+                sadw
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,23 @@ module.exports = {
   ],
   theme: {
     extend: {
-    },
-    colors:{
-      'primary' : '#8155FF',
-      'dark'  : '#4C4C54',
-      'secondary' : '#F8F5FF',
-      'footer-dark': '#061E30',
-    }, 
-    container: {
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+      colors:{
+        'primary' : '#8155FF',
+        'dark'  : '#4C4C54',
+        'secondary' : '#F8F5FF',
+        'footer-dark': '#061E30',
+      }, 
+      container: {
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem',
+        },
       },
     },
+   
   },
   plugins: [
       require('flowbite/plugin')
